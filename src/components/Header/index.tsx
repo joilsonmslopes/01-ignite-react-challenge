e85aec6059e35b5337a726d151004d191cbea2ec
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2023-01-14 16:37:40 -0300

    Add new rule on prettier and add Link to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
-import todoIcon from '/src/assets/rocket.svg'
+import todoIcon from '../../assets/rocket.svg'
 import style from './style.module.css'
 
 export const Header = () => {
-    return (
-        <div className={style.header}>
-            <Link to='/' className={style.logoWrapper}>
-                <img src={todoIcon} alt="Logo ToDo List" />
-                <h1>to<span>do</span></h1>
-            </Link>
-        </div>
-    )
+  return (
+    <div className={style.header}>
+      <Link to="/" className={style.logoWrapper}>
+        <img src={todoIcon} alt="Logo ToDo List" />
+        <h1>
+          to<span>do</span>
+        </h1>
+      </Link>
+    </div>
+  )
 }
